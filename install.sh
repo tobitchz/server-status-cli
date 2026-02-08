07c1841542bc930233a2e2009bc23525a0e3a811
tobi <tojoaca10@gmail.com>
2026-02-08 18:57:34 -0300

    add install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "instalando servercheck..."
+
+URL="https://raw.githubusercontent.com/tobitchz/server-status-cli/main/servercheck.py"
+DEST="/usr/local/bin/servercheck"
+
+sudo curl -L "$URL" -o "$DEST"
+sudo chmod +x "$DEST"
+
+echo "listo"
+echo "usa: servercheck"
